45db159bba82c0201bba7578b6d022a469251a2a
Ericks <esr@ecomp.poli.br>
2017-02-03 11:47:29 -0300

    Conexão Até Freguesias

--- assets/webservice.js
+++ assets/webservice.js
@@ -1,21 +1,19 @@
 var webservice = {
-	webMethod : "http://ws.era.pt/chatbotrest/api/",
 	doPost : function(da, op){
 		var retorno;
 		console.log(da);
         $.ajax({
 
 	        type: "GET",
-	        url: "https://api-era.herokuapp.com/erabot/distritos", 
+	        url: "https://api-era.herokuapp.com/erabot/" + op + "/" + da, 
 	        async: false,
 
-	        //data: Object.assign({},{token: "2f30fa79-28c0-44c9-8adb-f244a85cef27"},da),
+	       // data: da,
 	            success: function (msg) {
 	                var data = msg.hasOwnProperty("d") ? msg.d : msg;
 	                console.log
-	                var x2js = new X2JS();
 	                console.log(data);
-		            retorno = x2js.xml2json(data);
+		            retorno = data;
 	            },
 	            error: function (e) {
 
@@ -23,31 +21,6 @@ var webservice = {
 	            },
 	    });
 	    return retorno;
-	},
-	getConcelho: function(dist){
-		var retorno;
-		  $.ajax({
-	        type: "POST",
-	        url: this.webMethod, 
-	        async: false,
-
-	        data: {
-	        	token: "2f30fa79-28c0-44c9-8adb-f244a85cef27", 
-	        	distrito: dist
-	        },
-	            success: function (msg) {
-	                var data = msg.hasOwnProperty("d") ? msg.d : msg;
-	                var x2js = new X2JS();
-		            var concelhos = x2js.xml2json(data);
-		            retorno = distritos;
-	            },
-	            error: function (e) {
-
-	                console.log(e.responseText);
-	            },
-	    });
-
-		 return retorno;
 	}
 }
 
